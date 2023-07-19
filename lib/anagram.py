aa50dd9eaafa26d3713b5ed8b9f859779d3f2c10
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-07-19 15:35:06 -0700

    adds lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, old_list):
+        new_list = []
+        for word in old_list:
+            if sorted(letter for letter in word) == sorted(
+                letter for letter in self.word
+            ):
+                new_list.append(word)
+
+            else:
+                continue
+        return new_list
